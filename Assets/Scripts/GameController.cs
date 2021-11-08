0696b43a39b1d9e2a739c1410bb166637438bdcb
Luis Corales <luisaugcorales@gmail.com>
2021-11-08 15:41:11 -0500

    Add title and text per planet

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
@@ -8,28 +9,58 @@ public class GameController : MonoBehaviour
     public List<GameObject> Planets { get => planets; }
     GameObject currentPlanet;
     int count = 0;
+    string[] titlePlanet = new string[5];
+    string[] textPlanet = new string[5];
 
     [SerializeField] Camera cam;
     [SerializeField] float speed = 3f;
 
+    [SerializeField] Text title;
+    [SerializeField] Text text;
+
     void Start()
     {
         currentPlanet = Planets[count];
+
+        titlePlanet[0] = "Atracción";
+        textPlanet[0] = "Generar acciones que permitan vincular al mejor talento en base a la cultura UDLA.";
+
+        titlePlanet[1] = "Capacitación";
+        textPlanet[1] = "Brindar herramientas orientadas al desarrollo y fortalecimiento de conocimientos, habilidades y competencias.";
+
+        titlePlanet[2] = "Gestión de Talento";
+        textPlanet[2] = "Evaluar el desempeño y potencial de colaboradores UDLA para trabajar en estrategias de desarrollo.";
+
+        titlePlanet[3] = "Gestión de Estructura y Compensación";
+        textPlanet[3] = "Gestionar la estructura organizacional de la Universidad, que apalanque de manera efectiva los distintos procesos; y definir las pautas para el manejo del esquema remunerativo entre la Universidad y sus colaboradores.";
+
+        titlePlanet[4] = "Cultura y gestión de cambio";
+        textPlanet[4] = "Ejecutar y desarrollar acciones enfocadas a fortalecer la cultura y clima institucional facilitando los proccesos de gestión de cambio.";
     }
 
     void Update()
     {
-        if(count < 0) count = 0;
-        if(count > Planets.Count - 1) count = Planets.Count - 1;
+        if(count < 0)
+        {
+            count = 0;
+        }
+        
+        if(count > Planets.Count - 1)
+        {
+            count = Planets.Count - 1;
+        }
 
         if(Planets[count] != currentPlanet)
         {
             currentPlanet = Planets[count];
         }
 
-        Vector3 targetPosition = new Vector3(currentPlanet.transform.position.x, currentPlanet.transform.position.y, cam.transform.position.z);
+        Vector3 targetPosition = new Vector3(currentPlanet.transform.position.x + 3.5f, currentPlanet.transform.position.y, cam.transform.position.z);
 
         cam.transform.position = Vector3.MoveTowards(cam.transform.position, targetPosition, Time.deltaTime * speed);
+
+        title.text = titlePlanet[count];
+        text.text = textPlanet[count];
     }
 
     public void NextPlanet()
