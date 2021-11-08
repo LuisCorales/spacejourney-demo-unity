21f99f1f13f88edd7ee3e804f3f42d64501debe5
Luis Corales <luisaugcorales@gmail.com>
2021-11-08 13:56:36 -0500

    Add camera follow

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] List<GameObject> planets;
+    public List<GameObject> Planets { get => planets; }
+    GameObject currentPlanet;
+    int count = 0;
+
+    [SerializeField] Camera cam;
+    [SerializeField] float speed = 3f;
+
     void Start()
     {
-        
+        currentPlanet = Planets[count];
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if(count < 0) count = 0;
+        if(count > Planets.Count - 1) count = Planets.Count - 1;
+
+        if(Planets[count] != currentPlanet)
+        {
+            currentPlanet = Planets[count];
+        }
+
+        Vector3 targetPosition = new Vector3(currentPlanet.transform.position.x, currentPlanet.transform.position.y, cam.transform.position.z);
+
+        cam.transform.position = Vector3.MoveTowards(cam.transform.position, targetPosition, Time.deltaTime * speed);
+    }
+
+    public void NextPlanet()
+    {
+        count++;
+    }
+
+    public void PrevPlanet()
+    {
+        count--;
     }
 }
--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -7,21 +7,34 @@ public class UIController : MonoBehaviour
 {
     [SerializeField] RawImage background;
     bool isShowing = true;
+    [SerializeField] GameObject startButton;
+    [SerializeField] GameObject backButton;
 
     void Update() 
     {
         if (isShowing == true)
         {
-            background.CrossFadeAlpha(1, 1.0f, false);
+            //background.CrossFadeAlpha(1, 1.0f, false);
+            background.gameObject.SetActive(true);
         }
         if (isShowing == false)
         {
-            background.CrossFadeAlpha(0, 1.0f, false);
+            //background.CrossFadeAlpha(0, 1.0f, false);
+            background.gameObject.SetActive(false);
         }
     }
     
-    public void ToggleBackground()
+    public void StartButton()
     {
         isShowing = !isShowing;
+        startButton.SetActive(false);
+        backButton.SetActive(true);
+    }
+
+    public void BackButton()
+    {
+        isShowing = !isShowing;
+        backButton.SetActive(false);
+        startButton.SetActive(true);
     }
 }
